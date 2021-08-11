a64f4b1ce44b9e250d8f2df83c52dd9da2cc4af2
Vithushan <lvithu123@outlook.com>
2021-08-11 23:58:39 +0530

    Initial Commit

---
+++ FirstProject_App/ContentView.swift
@@ -0,0 +1,23 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            List{
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }.navigationBarTitle(Text("Storm Viewer"))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstProject_App/FirstProject_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProject_AppApp.swift
+//  FirstProject_App
+//
+//  Created by Vithushan Logan on 2021-08-11.
+//
+
+import SwiftUI
+
+@main
+struct FirstProject_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
